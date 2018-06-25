d1b3a83f590e259f99aecb869e820b409495e9f2
Artem Chernyaev <askanio234@gmail.com>
2018-06-25 20:13:55 +0300

    Implemented basic parsing for vacancy description

---
+++ parser.py
@@ -0,0 +1,43 @@
+import json
+import requests
+from bs4 import BeautifulSoup
+
+
+TARGET_URL = 'https://www.monster.com/jobs/q-financial-analyst-jobs.aspx?page=1'
+
+MAIN_DATA_KEY = 'mainEntityOfPage'
+
+MAX_PAGE = 2
+
+
+def get_html_page(url):
+    request = requests.get(url)
+    return request.text
+
+
+def get_vacancies_data(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    scripts = soup.find_all('script', type='application/ld+json')
+    for script in scripts:
+        jsonifed_data = json.loads(script.get_text())
+        if MAIN_DATA_KEY in jsonifed_data.keys():
+            return jsonifed_data
+
+
+def get_vacancies_urls(json_data):
+    return [position['url'] for position in json_data['itemListElement']]
+
+
+def get_vacancy_description(url):
+    html = get_html_page(url)
+    soup = BeautifulSoup(html, 'html.parser')
+    script = soup.find('script', type='application/ld+json')
+    return json.loads(script.get_text())['description']
+
+
+if __name__ == '__main__':
+    html = get_html_page(TARGET_URL)
+    vacancies_data = get_vacancies_data(html)
+    vacancies_urls = get_vacancies_urls(vacancies_data)
+    desc = get_vacancy_description(vacancies_urls[0])
+    print(desc)
